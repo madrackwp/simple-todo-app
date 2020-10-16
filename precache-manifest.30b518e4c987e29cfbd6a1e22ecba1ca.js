d0d30e28dc8180fd682772d612e980f25a361fb4
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-10-16 21:44:46 +0800

    Updates

---
+++ precache-manifest.30b518e4c987e29cfbd6a1e22ecba1ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48109d95d46aa36a9f204a0f42e79077",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "29a232ee1a5898afbc0f",
+    "url": "/simple-todo-app/static/css/main.10b0e4ea.chunk.css"
+  },
+  {
+    "revision": "86df2e00d991f56aa702",
+    "url": "/simple-todo-app/static/js/2.72139e7b.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/simple-todo-app/static/js/2.72139e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a232ee1a5898afbc0f",
+    "url": "/simple-todo-app/static/js/main.40074a1e.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
